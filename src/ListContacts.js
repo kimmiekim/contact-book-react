dc7502f8ec42556253bac9acf33064c92e4b40d1
Erin Kim <kim.erin.kim@gmail.com>
2017-09-12 15:31:53 -0400

    search bar that changes the state of list contact component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,37 +1,48 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class ListContacts extends Component {
   static propTypes = {
     contacts: PropTypes.array.isRequired
   }
-  render(){
+  state= {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
+  render() {
     const contacts = this.props.contacts
     let showingContacts = contacts
     // {console.log(JSON.stringify(showingContacts))}
 
-
-    return(
+    return (
       <div>
-        voila list contacts
-        <ol className = 'contact-list'>
-        {showingContacts.map((contact) => (
-          <li key={contact.id} className = 'contact-list-item'>
-            <div className='contact-avatar' style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }} />
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-          </li>
-        ))}
-    </ol>
-  </div>
+        <input
+          className = "search-contact"
+          type = "text"
+          placeholder = "search-contact"
+          value = {this.state.query}
+          onChange = {(event) => this.updateQuery(event.target.value)}
+          />
+          {JSON.stringify(this.state)}
 
-
-
-
+        <ol className="contact-list">
+          {showingContacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+              <div className="contact-avatar" style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}/>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
 
     )
   }
